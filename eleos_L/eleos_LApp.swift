b4de751de796ba251114ca0cd76fb84b459d5ceb
toksJ <tokajaber11@gmail.com>
2023-08-21 18:37:13 +0400

    Initial Commit

---
+++ eleos_L/eleos_LApp.swift
@@ -0,0 +1,17 @@
+//
+//  eleos_LApp.swift
+//  eleos_L
+//
+//  Created by TokaJaber on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct eleos_LApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
