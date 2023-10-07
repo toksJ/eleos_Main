358d6b758c02f2721126259d6f825c9fb98a9e05
toksJ <tokajaber11@gmail.com>
2023-10-08 01:00:12 +0400

    home page clouds

---
+++ eleos_L/HomeLeget/HomeWidget.swift
@@ -0,0 +1,64 @@
+//
+//  HomeWidget.swift
+//  eleos_L
+//
+//  Created by TokaJaber on 08/10/2023.
+//
+
+import SwiftUI
+
+struct HomeWidget: View {
+    var forecast: Forecast
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Trapezoid()
+                .fill(Color.weatherWidgetBackground)
+                .frame(width: 342, height: 174)
+            
+            HStack(alignment: .bottom) {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Reflicting")
+                        .font(.system(size: 35))
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text("more drops more life")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                        
+                        
+                        Text(forecast.location)
+                            .font(.body)
+                            .lineLimit(1)
+                    }
+                    
+                }
+                Spacer()
+                
+                VStack(alignment: .trailing, spacing: 0) {
+                    Image("\(forecast.icon) large")
+                        .padding(.trailing, 4)
+                    
+                    
+                    Text("how do i reflect?")
+                        .font(.footnote)
+                        .padding(.trailing, 24)
+                }
+                
+            }
+            .foregroundColor(.white)
+            .padding(.bottom, 20)
+            .padding(.leading, 20)
+        }
+        
+        .frame(width: 342, height: 184, alignment: .bottom)
+        
+    }
+}
+
+struct HomeWidget_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeWidget(forecast: Forecast.cities[0])
+            .preferredColorScheme(.dark)
+    }
+}
+
---
+++ eleos_L/HomeLeget/Scrollers.swift
@@ -0,0 +1,42 @@
+//
+//  Scrollers.swift
+//  eleos_L
+//
+//  Created by TokaJaber on 08/10/2023.
+//
+
+import SwiftUI
+
+struct Scrolleres: View {
+    var body: some View {
+        ZStack {
+            Color.background
+                .ignoresSafeArea()
+            
+            
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    ForEach(Forecast.cities) { forecast in
+                        HomeWidget(forecast: forecast)
+                    }
+                }
+            }
+            .safeAreaInset(edge: .top) {
+                EmptyView()
+                    .frame(height: 110)
+            }
+        }
+        .overlay {
+            EloesHome()
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct Scrolleres_Previews: PreviewProvider {
+    static var previews: some View {
+        Scrolleres()
+            .preferredColorScheme(.dark )
+    }
+}
+
