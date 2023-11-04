bae3bda007746cfa74dd388b67c651d04f7b8544
Razan Raid <razanraid990@gmail.com>
2023-11-04 18:52:23 +0400

    plant progress view 2. idk what causes the crash

---
+++ ProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressBar.swift
+//  eleos_L
+//
+//  Created by Razan Raid on 04/11/2023.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressBar()
+}
